20692ce60b7e5e0480c64233b64894373bb65787
Fredrik Tillman <fredrikt@activesolution.se>
2021-12-08 10:10:58 +0100

    8a

--- 2021/8/Day8.cs
+++ 2021/8/Day8.cs
@@ -4,13 +4,35 @@ public static class Day8{
         B();
     }
 
-    private static void A(){
+    private static IEnumerable<Entry> GetInput(){
+        //var stringList = FileHelper.ReadInput("2021/8/exampleInput.txt");
+        var stringList = FileHelper.ReadInput("2021/8/input.txt");
+
+        foreach(var line in stringList){
+            var parts = line.Split(" | ");
+            yield return new Entry{ Patterns = parts[0].Split(' '), Output = parts[1].Split(' ')};
+        }     
         
-        Console.WriteLine("8a: "+null);
     }
+
+    private static void A(){
+        var entries = GetInput();
+        var allOutputs = entries.SelectMany(e => e.Output);
+        var lengths = new[] {2,3,4,7};
+        var count = allOutputs.Count(p => lengths.Contains(p.Length));
+
+        Console.WriteLine("8a: "+count);
+    }
+
+    
 
     private static void B(){
         
         Console.WriteLine("8b: "+null);
+    }
+
+    private class Entry{
+        public string[] Patterns { get; set; }
+        public string[] Output { get; set; }
     }
 }
