59a56fe10ef03d955bf3fbc1e214a7336098755d
Fredrik Tillman <fredrikt@activesolution.se>
2021-12-11 17:18:37 +0100

    9b

--- 2021/9/Day9.cs
+++ 2021/9/Day9.cs
@@ -54,18 +54,96 @@ public static class Day9{
         Console.WriteLine("9a: "+result);
     }
 
-    private static int FindBasins(Dictionary<int, int[]> input){
+    private static List<Point> GetAllPoints(Dictionary<int, int[]> input){
+        var result = new List<Point>();
         
-        var evaluatedPoints = new Dictionary<int, int>();
-        
+        foreach(var row in input){
+            var rowIndex = row.Key;
 
-        return 0;
+            var heights = row.Value;
+
+            for(int columnIndex = 0; columnIndex < heights.Length; columnIndex++){
+                var point = GetPoint(input, rowIndex, columnIndex);
+                
+                result.Add(point);
+            }
+        }
+
+        return result;
+    }
+
+    private static Point GetPoint(Dictionary<int, int[]> input, int row, int column){
+        var currentRow = input[row];
+        var prevRow = input.ContainsKey((int)(row - 1)) ? input[(int)(row - 1)] : null;
+        var nextRow = input.ContainsKey((int)(row + 1)) ? input[(int)(row + 1)] : null;
+
+
+        int value=currentRow[column],top=10,left=10,right=10,bottom=10;
+
+        if(prevRow != null){
+            top = prevRow[column];
+        }
+        if(nextRow != null){
+            bottom = nextRow[column];
+        }
+        if(column > 0)
+            left = currentRow[column-1];
+        if(column < currentRow.Length-1)
+            right = currentRow[column+1];
+        
+        return new Point(value, top, left, right, bottom, row, column);
+    }
+
+    private static List<Point> FindNonNineNotEvaluatedNeigbours(Point point, List<Point> allPoints, List<Point> basin){
+        if(point.IsEvaluated)
+            return basin;
+        
+        basin.Add(point);
+
+        allPoints.First(p => p.Row == point.Row && p.Column == point.Column).IsEvaluated = true;
+
+        if(point.TopNeighbour < 9){
+            var top = allPoints.First(p => p.Row == point.Row-1 && p.Column == point.Column);
+            basin = FindNonNineNotEvaluatedNeigbours(top, allPoints, basin);
+        }
+        if(point.BottomNeighbour < 9){
+            var bottom = allPoints.First(p => p.Row == point.Row+1 && p.Column == point.Column);
+            basin = FindNonNineNotEvaluatedNeigbours(bottom, allPoints, basin);
+        }
+        if(point.LeftNeigbour < 9){
+            var left = allPoints.First(p => p.Row == point.Row && p.Column == point.Column-1);
+            basin = FindNonNineNotEvaluatedNeigbours(left, allPoints, basin);
+        }
+        if(point.RightNeighbour < 9){
+            var right = allPoints.First(p => p.Row == point.Row && p.Column == point.Column+1);
+            basin = FindNonNineNotEvaluatedNeigbours(right, allPoints, basin);
+        }
+
+
+        return basin;
+    }
+
+    private static int FindBasinsSizeSum(Dictionary<int, int[]> input){
+        var lowpoints = FindLowpoints(input);
+        var allPoints = GetAllPoints(input);
+
+        var basinSizes = new List<int>();
+        foreach(var lowpoint in lowpoints){
+            basinSizes.Add(FindNonNineNotEvaluatedNeigbours(lowpoint, allPoints, new List<Point>()).Count());
+        }
+        int result = 1;
+
+        foreach(var size in basinSizes.OrderByDescending(b => b).Take(3)){
+            result = result * size;
+        }
+
+        return result;
     }
 
     private static void B(){
         var input = GetInput();
-        var basins = FindBasins(input);
-        Console.WriteLine("9b: "+null);
+        var sum = FindBasinsSizeSum(input);
+        Console.WriteLine("9b: "+sum);
     }
 
     private class Point{
@@ -87,5 +165,7 @@ public static class Day9{
         public int Row { get; set; }
         public int Column { get; set; }
         public int RiskLevel => Value + 1;
+        public bool IsEvaluated { get; set; }
+        public bool IsLowpoint => Value < TopNeighbour && Value < LeftNeigbour && Value < RightNeighbour && Value < BottomNeighbour;
     }
 }
