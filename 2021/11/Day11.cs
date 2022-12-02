21bbeb488b9e7f08b76e7599c0af276415c7941a
fredrik.tillman <fredrik.tillman@gmail.com>
2022-12-02 14:18:19 +0100

    some old change :)

--- 2021/11/Day11.cs
+++ 2021/11/Day11.cs
@@ -4,13 +4,32 @@ public static class Day11{
         B();
     }
 
+    private static Dictionary<(int,int),Octopus> GetInput(){
+        var stringList = FileHelper.ReadInput("2021/11/exampleInput.txt");
+        //stringList = FileHelper.ReadInput("2021/10/input.txt");
+        var returnValue = new Dictionary<(int,int),Octopus>();
+        for(int i = 0; i < stringList.Count(); i++){
+            var intList = stringList[i].ToCharArray().Select(x => int.Parse(x.ToString())).ToList();
+            for(int j = 0; j < intList.Count(); j++){
+                returnValue.Add((i,j),new Octopus{ Value = intList[j]});
+            }
+        }
+
+        return returnValue;
+    }
+
     private static void A(){
-        
+        var octopuses = GetInput();        
         Console.WriteLine("11a: "+null);
     }
 
     private static void B(){
         
         Console.WriteLine("11b: "+null);
+    }
+
+    private class Octopus{
+        public int Value { get; set; }
+        public bool HasFlashed { get; set; }
     }
 }
